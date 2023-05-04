5b380e79e511c6a187cefa417be844aa50a1d297
@code <@code>
2023-05-04 12:20:30 +0700

    mediaScreen progress

--- js/profile.js
+++ js/profile.js
@@ -5,39 +5,48 @@ window.onscroll = () => {
   let cssBar = document.getElementById('css-bar');
   let bootstrapBar = document.getElementById('bootstrap-bar');
   let javaScriptBar = document.getElementById('javascript-bar');
-  if(window.scrollY < 300){
-    for(i = 3; i >= 1 ; i--){
+  if (window.scrollY < 300) {
+    for (i = 3; i >= 1; i--) {
       introductionSpan[i].style.marginLeft = '-100vw';
       introductionSpan[i].style.transition = `all ${i}s`;
     }
-    animationBar(htmlBar,0);
-    animationBar(cssBar,0);
-    animationBar(bootstrapBar,0);
-    animationBar(javaScriptBar,0);
-  }else{
+    animationBar(htmlBar, 0);
+    animationBar(cssBar, 0);
+    animationBar(bootstrapBar, 0);
+    animationBar(javaScriptBar, 0);
+  } else {
     introductionElement.style.opacity = 1;
     introductionElement.style.transition = 'opacity .5s';
-    for(i = 1; i < 4; i++){
+    for (i = 1; i < 4; i++) {
       introductionSpan[i].style.marginLeft = '1vw';
-      introductionSpan[i].style.transition = `all ${i+1}s`;
+      introductionSpan[i].style.transition = `all ${i + 1}s`;
     }
-    setTimeout(function() {
-    animationBar(htmlBar,50);
-    animationBar(cssBar,50);
-    animationBar(bootstrapBar,50);
-    animationBar(javaScriptBar,50);
+    setTimeout(function () {
+      animationBar(htmlBar, 50);
+      animationBar(cssBar, 50);
+      animationBar(bootstrapBar, 50);
+      animationBar(javaScriptBar, 50);
     }, 3000);
   }
 }
 
-function animationBar(element,value){
-  for(i=0; i<=value; i++){
+function animationBar(element, value) {
+  for (i = 0; i <= value; i++) {
     element.style.transition = 'width 20s';
-    element.setAttribute('style',`width: ${i}%`);
+    element.setAttribute('style', `width: ${i}%`);
   }
-  if(i>=50 && i<75){
+  if (i >= 50 && i < 75) {
     element.textContent = 'Intermediate';
   }
 }
 
-
+function mediaScreen(x) {
+  let navbar = document.getElementById('navbar');
+  if (x.matches) {
+    navbar.classList.remove('justify-content-center');
+    navbar.classList.add('justify-content-end');
+  }
+}
+var screenWidth = window.matchMedia('(min-width: 1280px)');
+mediaScreen(screenWidth);
+screenWidth.addListener(mediaScreen)
